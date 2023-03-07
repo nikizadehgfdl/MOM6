f7f0af475e3c7ab49e0e6755721983ad039d2d81
Niki Zadeh <niki.zadeh@noaa.gov>
2023-03-07 14:05:32 -0500

    Fix issue for GNU compiler
    - GNU executable crashes on send_data3d because it cannot handle
      optional input arguments that are declared as class(*) inside the
      callee interface, in this case rmask inside diag_manager::send_data_3d
      Such optional arguments have to be checked for and passed explicitly
      to send_data_3d
    - This is similar to the issue in https://github.com/NOAA-GFDL/MOM6/pull/68

--- config_src/infra/FMS2/MOM_diag_manager_infra.F90
+++ config_src/infra/FMS2/MOM_diag_manager_infra.F90
@@ -345,8 +345,17 @@ logical function send_data_infra_3d(diag_field_id, field, is_in, ie_in, js_in, j
   character(len=*),          optional, intent(out) :: err_msg !< A log indicating the status of the post upon
                                                            !! returning to the calling routine
 
-  send_data_infra_3d = send_data_fms(diag_field_id, field, time, is_in, js_in, ks_in, mask, &
-                               rmask, ie_in, je_in, ke_in, weight, err_msg)
+  if (present(mask) .and. present(rmask)) then
+    send_data_infra_3d = send_data_fms(diag_field_id, field, time=time, is_in=is_in, js_in=js_in, ks_in=ks_in, &
+                               mask=mask, rmask=rmask, ie_in=ie_in, je_in=je_in, ke_in=ke_in, weight=weight, &
+                               err_msg=err_msg)
+  elseif (present(rmask)) then
+    send_data_infra_3d = send_data_fms(diag_field_id, field, time=time, is_in=is_in, js_in=js_in, ks_in=ks_in, &
+                               rmask=rmask, ie_in=ie_in, je_in=je_in, ke_in=ke_in, weight=weight, err_msg=err_msg)
+  else
+    send_data_infra_3d = send_data_fms(diag_field_id, field, time=time, is_in=is_in, js_in=js_in, ks_in=ks_in, &
+                               ie_in=ie_in, je_in=je_in, ke_in=ke_in, weight=weight, err_msg=err_msg)
+  endif                                                           
 
 end function send_data_infra_3d
 
